a8ffd9a0ffd85f8f42f858047f8ca3598814cc0b
Willie <write2willie@gmail.com>
2021-05-13 16:16:35 -0400

    commit #10: changed server.js, added routes, may132021, 4.16pm

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,37 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  const categoryData = await Category.findAll().catch((err) => {
+    res.status(500).json(err)
+  });
+  res.status(200).json(categoryData);
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+
+//come back and review this one
+router.get('/:id', async (req, res) => {
+
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // where: {
+      //   id: req.params.id;
+      // }
+      // JOIN with travellers, using the Trip through table
+      //include: [{ model: Traveller, through: Trip, as: 'location_travellers' }]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,21 +3,21 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
 });
 
