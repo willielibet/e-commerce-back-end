4665695988c91ae695a38acb444152bcc516474b
Willie <write2willie@gmail.com>
2021-05-14 14:07:18 -0400

    commit #22: fixed and added comments, may142021, 2.07pm

--- models/index.js
+++ models/index.js
@@ -4,26 +4,20 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+
 Product.belongsTo(Category, {
-  foreignKey: "category_id",
+  foreignKey: 'category_id',
 });
-
-// Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "category_id",
+  foreignKey: 'category_id',
 });
-
-// Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: "product_id",
+  foreignKey: 'product_id',
 });
-
-// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: "tag_id",
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync sequelize models to the database, then turn on the server.
+// Force true to drop/recreate table(s) on every sync.
 sequelize.sync({ force: false}).then(() => {
   app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
