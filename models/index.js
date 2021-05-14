22f9bce65520c223e86e4e086b308f944454c431
Willie <write2willie@gmail.com>
2021-05-14 15:44:02 -0400

    commit #24: saved models/index.js file, may142021, 3.44pm

--- models/index.js
+++ models/index.js
@@ -8,13 +8,16 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
+
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
+
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
 });
+
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
