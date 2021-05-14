82a02a566a4bc62d9e33a1189be46143bee61e8c
Willie <write2willie@gmail.com>
2021-05-13 21:27:39 -0400

    commit #14: added route get to find all categories, may132021, 9.27pm

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,14 +2,24 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
+ //attributes: ['product_name']
 router.get('/', async (req, res) => {
+  try {
   // find all categories
-  const categoryData = await Category.findAll().catch((err) => {
-    res.status(500).json(err)
-  });
-  res.status(200).json(categoryData);
-  // be sure to include its associated Products
+  const categoryData = await Category.findAll({
+      // be sure to include its associated Products
+      include: [
+        { 
+          model: Product,
+      attributes: ['id','product_name','price','stock','category_id'], 
+    },
+  ],
+    });
+
+    res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 
@@ -23,8 +33,12 @@ router.get('/:id', async (req, res) => {
       // where: {
       //   id: req.params.id;
       // }
-      // JOIN with travellers, using the Trip through table
-      //include: [{ model: Traveller, through: Trip, as: 'location_travellers' }]
+      include: [
+        { 
+          model: Product,
+          attributes: ['id','product_name','price','stock','category_id'], 
+    },
+  ],
     });
     if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' });
@@ -38,6 +52,7 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+
 });
 
 router.put('/:id', (req, res) => {
