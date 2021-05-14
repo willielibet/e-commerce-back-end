7d870aa6d3485b26811394d3141c9831001aa235
Willie <write2willie@gmail.com>
2021-05-14 11:15:59 -0400

    commit #19: added code to create a new category (post), may142021, 11.15am

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -30,9 +30,6 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      // where: {
-      //   id: req.params.id;
-      // }
       include: [
         { 
           model: Product,
@@ -52,8 +49,14 @@ router.get('/:id', async (req, res) => {
 
 
 //POST
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 
 });
 
@@ -61,8 +64,24 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
